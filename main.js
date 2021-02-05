93b1010666d3d19c4639d4065bbb3c8798dd07c5
KIMJIHYE <crk33333@gmail.com>
2021-02-05 15:04:18 +0900

    argument, NotFound

--- main.js
+++ main.js
@@ -5,19 +5,14 @@ var url = require('url'); //모듈 url임
 var app = http.createServer(function(request,response){
     var _url = request.url; //주소의 url을 가져옴
     var queryData = url.parse(_url, true).query; //url의 파라미터 객체
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
 
 
-    console.log('queryData.name:'+queryData.name); //파라미터를 .파라미터이름 을 통해 가져옴
-    console.log('queryData.id:'+queryData.id);
+    console.log(queryData);
+    console.log(url.parse(_url,true));
 
-    if(_url == '/'){//홈으로 갔을 때
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
+if(pathname ==='/'){//경로에 제대로 접근했을 ;
     fs.readFile(`data/${queryData.id}`, 'utf-8',
     function(err, description){
 
@@ -41,9 +36,15 @@ var app = http.createServer(function(request,response){
       </html>
 
       `;
-      response.end(template); //출력 화면
-    }) //디렉토리 내의 파일을 불러옴
 
+      response.writeHead(200); //200은 파일이 성공적으로 전송되었다는 의미
+      response.end(template); //출력 화면
+    }); //디렉토리 내의 파일을 불러옴
+  } else{
+    response.writeHead(404); //잘못된 경로로 접근했을 때.
+    response.end('Not Found'); //출력 화면
+
+  }
 
 
 
--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,9 +1,13 @@
+
+var args = process.argv;
+
+console.log(args[2]);
 console.log('A');
 console.log('B');
 
-if(false){
+if(args[2] === '1'){ //input에 1을 입력시 실행됨
   console.log('C1');
-}else {
+} else {             //1이 아닌 다른 값 입력시 실행
   console.log('C2');
 }
 console.log('D');
